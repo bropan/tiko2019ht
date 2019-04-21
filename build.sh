ada0e6e4aaacf6bbf258110d4af488c94138582f
bropan <bropan@noemail.com>
2019-04-21 20:33:59 +0300

    Added build and run bash scripts
    Bash scripts for compiling the source and running the program.
    
    To use in linux:
    
    type
    ./build.sh
    ./run.sh
    ./buildrun.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+javac                   \
+        src/Main.java   \
+        src/Init.java   \
+        src/CLI.java    \
+        src/Utils.java  \
+        src/LoginCredentials.java \
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+java -cp '.:driver/*:src' Main
+
