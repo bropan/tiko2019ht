bf84610d1d9d449d8dd0c2ad8fd56835df84dab0
bropan <bropan@noemail.com>
2019-04-23 12:08:24 +0300

    Add functions for interactive add
    Added addInteractive(), enterColumns(), enterColumn()
    Not functional yet.

--- src/CLI.java
+++ src/CLI.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import java.sql.SQLException;
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 import java.io.PrintWriter;
 import java.io.IOException;
 public class CLI {
     private final static String QUIT = "quit";
+    private final static String ADD = "add";
     private final static String HELP = "help";
     private final static String WRITE_CREDENTIALS = "createlogin";
     private final static String RESET_DATABASE = "resetdatabase";
@@ -46,6 +52,9 @@ public class CLI {
                 case QUIT:
                     continueRunning = false;
                     break;
+                case ADD:
+                    addInteractive(userInput, con);
+                    break;
                 case WRITE_CREDENTIALS:
                     writeCredentialsToFile(userInput, Main.LOGIN_CONFIG_FILE_PATH);
                     break;
@@ -69,6 +78,8 @@ public class CLI {
 
                 QUIT + " - Exit program"
                 + "\n" +
+                ADD + " - Add something"
+                + "\n" +
                 WRITE_CREDENTIALS + " - Create/update a login configuration file" 
                 + "\n" +
                 RESET_DATABASE  + " - Reset database (warning: removes all entered data!)"
@@ -78,6 +89,24 @@ public class CLI {
 
                 "----- Commands -----" + "\n\n" 
         );
+    }
+
+    public static void addInteractive(Scanner userInput, Connection con) throws SQLException {
+        ArrayList<String> addOptions = new ArrayList<>(Arrays.asList(
+            "asiakas",
+            "tyokohde",
+            "tyosuoritus",
+            "laskutettava"
+        ));
+        int answer = askSelection("Select what needs to be added ", addOptions, userInput);
+        if(answer >= 0){
+            String tableName = addOptions.get(answer);
+            System.out.println("You answered " + tableName);
+            enterColumns(userInput, tableName, con);
+        } else {
+            return;
+        }
+
     }
 
     public static LoginCredentials loginFromUserInput(Scanner userInput){
@@ -114,6 +143,73 @@ public class CLI {
         if(confirm){
             DatabaseStructureHandler.resetDatabase(con, Main.GLOBAL_SCHEMA); 
         }
+    }
+
+    //returns false if aborted
+    private static boolean enterColumns(Scanner userInput, String tableName, Connection con) throws SQLException {
+        Statement getColumns = con.createStatement();
+        ResultSet rs = getColumns.executeQuery("SELECT * FROM "+tableName);
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnCount = rsmd.getColumnCount();
+        System.out.println(tableName + " has " + columnCount + " columns");
+        for(int i=1; i <= columnCount; ++i){
+            enterColumn(userInput,rs,i,con);
+        }
+
+        return true;
+    }
+    
+    private static boolean enterColumn(Scanner userInput, ResultSet rs, int column, Connection con) throws SQLException {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        String label = rsmd.getColumnLabel(column);
+        String name = rsmd.getColumnName(column);
+        System.out.println(label + " " + name);
+        return true;
+    }
+
+    public static int askSelection(String question, ArrayList<String> options, Scanner userInput){
+        int selection = -1;
+        System.out.println(question);
+        int optionsLength = options.size();
+        if(question==null||options==null||optionsLength==0||userInput==null){
+            throw new IllegalArgumentException("Invalid parameters for askSelection!");
+        }
+
+        for(int i=0; i < optionsLength; ++i){ //Listing options
+            System.out.println("    " + "[" + (i+1) + "] " + options.get(i));
+        }
+        System.out.println("    " + "[a] ABORT");
+
+        boolean asking = true;
+        while(asking){
+            System.out.println("Enter choice [1-"+optionsLength+"]: ");
+            String answer = userInput.nextLine();
+            try {
+                int asNumber = Integer.parseInt(answer) - 1;
+                if(asNumber >= 0 && asNumber < optionsLength){
+                    selection = asNumber;
+                    asking = false;
+                } else {
+                    System.out.println("Select a number from range [1-"+optionsLength+"]!");
+                }
+            } catch (NumberFormatException nfe){
+                if(answer.equals("ABORT") || answer.equals("a")){
+                    selection = -1;
+                    asking = false;
+                } else {
+
+                    int search = options.indexOf(answer);
+                    if(search != -1){
+                        selection = search;
+                        asking = false;
+                    } else {
+                        System.out.println("Invalid input, "
+                                +"type a number or the name of the selection!");
+                    }
+                }
+            }
+        }
+        return selection;
     }
 
     public static String askUser(String question, Scanner s){
